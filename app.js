d626d3115cee4c8a543e1a88d0a599da062c46e0
DESKTOP-DQG7J9S\ronan <ronan@roquais.fr>
2023-06-26 18:03:10 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,97 @@
+import 'dotenv/config';
+import express from 'express';
+import {
+  InteractionType,
+  InteractionResponseType,
+  InteractionResponseFlags,
+  MessageComponentTypes,
+  ButtonStyleTypes,
+} from 'discord-interactions';
+import * as chrono from 'chrono-node';
+import { VerifyDiscordRequest, getRandomEmoji, DiscordRequest } from './utils.js';
+import { getShuffledOptions, getResult } from './game.js';
+
+const msToTimestamp = (ms) => {
+  const msAsString = ms.toString();
+  return msAsString.substring(0, msAsString.length-3);
+}
+
+function start() {
+// Create an express app
+const app = express();
+// Get port, or default to 3000
+const PORT = process.env.PORT || 3000;
+// Parse request body and verifies incoming requests using discord-interactions package
+app.use(express.json({ verify: VerifyDiscordRequest(process.env.PUBLIC_KEY) }));
+
+// Store for in-progress games. In production, you'd want to use a DB
+const activeGames = {};
+
+/**
+ * Interactions endpoint URL where Discord will send HTTP requests
+ */
+app.post('/interactions', async function (req, res) {
+  // Interaction type and data
+  const { type, id, data } = req.body;
+
+  /**
+   * Handle verification requests
+   */
+  if (type === InteractionType.PING) {
+    return res.send({ type: InteractionResponseType.PONG });
+  }
+
+  /**
+   * Handle slash command requests
+   * See https://discord.com/developers/docs/interactions/application-commands#slash-commands
+   */
+  if (type === InteractionType.APPLICATION_COMMAND) {
+    const { name, options } = data;
+
+    // "test" command
+    if (name === 'test') {
+      // Send a message into the channel where command was triggered from
+      return res.send({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+          // Fetches a random emoji to send from a helper function
+          content: 'hello world ' + getRandomEmoji(),
+        },
+      });
+    }
+
+    console.log(data)
+    if (name === 'now') {
+      const timestamp = msToTimestamp(Date.now())
+      return res.send({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+          content: `<t:${timestamp}:F>`
+        }
+      })
+    }
+
+    if( name === "timestamp") {
+      const [date] = options
+      const parsedDate = chrono.parseDate(date.value)
+      console.log(parsedDate)
+      const timestamp = msToTimestamp(Date.now())
+      console.log(timestamp)
+      const doubleParse = msToTimestamp(Date.parse(parsedDate))
+      console.log(doubleParse)
+      return res.send({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+          content: `<t:${doubleParse}:F>`
+        }
+      })
+    }
+  }
+});
+
+app.listen(PORT, () => {
+  console.log('Listening on port', PORT);
+});
+}
+
+start()
---
+++ commands.js
@@ -0,0 +1,47 @@
+import 'dotenv/config';
+import { getRPSChoices } from './game.js';
+import { capitalize, InstallGlobalCommands } from './utils.js';
+
+// Get the game choices from game.js
+function createCommandChoices() {
+  const choices = getRPSChoices();
+  const commandChoices = [];
+
+  for (let choice of choices) {
+    commandChoices.push({
+      name: capitalize(choice),
+      value: choice.toLowerCase(),
+    });
+  }
+
+  return commandChoices;
+}
+
+// Simple test command
+const TEST_COMMAND = {
+  name: 'test',
+  description: 'Basic command',
+  type: 1,
+};
+
+const NOW = {
+  name: 'now',
+  description: 'Gives the current time as a timestamp',
+  type: 1
+}
+
+const TIMESTAMP = {
+  name: 'timestamp',
+  description: 'Send a date, get the timestamp',
+  type: 1,
+  options: [{
+    type: 3,
+    name: 'date',
+    description: "The date you'd like to convert to a timestamp",
+    required: true
+  }]
+}
+
+const ALL_COMMANDS = [TEST_COMMAND, NOW, TIMESTAMP];
+
+InstallGlobalCommands(process.env.APP_ID, ALL_COMMANDS);
